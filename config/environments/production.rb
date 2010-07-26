bdce6679cf571468a52533de22b34057c9c9defc
Jake <github@jake.otherinbox.com>
2010-07-25 22:28:55 -0700

    Updated mail settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,4 +27,4 @@ config.action_view.cache_template_loading            = true
 # Enable threaded mode
 # config.threadsafe!
 
-ActionMailer::Base.delivery_method = :smtp
+config.action_mailer.delivery_method = :smtp
